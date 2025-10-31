7de45b6080bf1fce7cebbe133c1c39d7fa17f33d
Andrew Xiong <andrewxiong@ucsb.edu>
2025-10-30 17:18:44 -0700

    added fixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,38 @@
+const articlesFixtures = {
+    oneArticle: {
+        id: 1,
+        title: "guyfalloffcliff",
+        url: "www.news.com/guyfalloffcliff",
+        email: "joesmith@email.com",
+        explanation: "guy falls off cliff",
+        localDateTime: "2022-01-02T12:00:00",
+    },
+    threeArticles: [
+        {
+            id: 1,
+            title: "guyfalloffcliff",
+            url: "www.news.com/guyfalloffcliff",
+            email: "joesmith@email.com",
+            explanation: "guy falls off cliff",
+            localDateTime: "2022-01-02T12:00:00",
+        },
+        {
+            id: 2,
+            title: "guygetsinjured",
+            url: "www.news.com/guygetsinjured",
+            email: "joesmith@email.com",
+            explanation: "guy gets injured",
+            localDateTime: "2022-04-03T12:00:00",
+        },
+        {
+            id: 3,
+            title: "guyrecovers",
+            url: "www.news.com/guyrecovers",
+            email: "joesmith@email.com",
+            explanation: "guy recovers",
+            localDateTime: "2022-07-04T12:00:00",
+        },
+    ],
+};
+
+export { articlesFixtures };
