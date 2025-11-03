16155a6b295b24a57e3866a412744e8816179b23
Jayden Li <jaydenli@ucsb.edu>
2025-11-03 13:34:58 -0800

    jl - create fixtures for menuitemreview table

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,38 @@
+const menuItemReviewFixtures = {
+  oneMenuItemReview: {
+    id: 1,
+    itemId: 1,
+    reviewerEmail: "jaydenli@ucsb.edu",
+    stars: 5,
+    dateReviewed: "2025-11-03T12:59:59",
+    comments: "yummy food!",
+  },
+  threeMenuItemReviews: [
+    {
+      id: 1,
+      itemId: 1,
+      reviewerEmail: "jaydenli@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2025-11-03T12:59:59",
+      comments: "yummy food!",
+    },
+    {
+      id: 2,
+      itemId: 2,
+      reviewerEmail: "tvn@ucsb.edu",
+      stars: 2,
+      dateReviewed: "2025-11-02T12:59:59",
+      comments: "ts was lwk buns.",
+    },
+    {
+      id: 3,
+      itemId: 1,
+      reviewerEmail: "akul@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2025-11-02T10:49:59",
+      comments: "amazing!",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
