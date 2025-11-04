05032defd30e722ef423e3caa16385cc0cded104
Andrew Xiong <andrewxiong@ucsb.edu>
2025-11-04 00:55:20 -0800

    created edit page and tests

--- frontend/src/main/components/Articles/ArticleForm.jsx
+++ frontend/src/main/components/Articles/ArticleForm.jsx
@@ -16,13 +16,13 @@ function ArticleForm({
   // Stryker restore all
 
   const navigate = useNavigate();
-  // ✅ Defined before use
+
+  const testIdPrefix = "ArticleForm";
+
   // Stryker disable Regex
   const isodate_regex =
     /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
   // Stryker restore Regex
-
-  const testIdPrefix = "ArticleForm";
 
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
@@ -63,6 +63,7 @@ function ArticleForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="url">URL</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-url"}
           id="url"
           type="text"
           isInvalid={Boolean(errors.url)}
@@ -78,6 +79,7 @@ function ArticleForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="email">Email</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-email"}
           id="email"
           type="text"
           isInvalid={Boolean(errors.email)}
@@ -93,6 +95,7 @@ function ArticleForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="explanation">Explanation</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-explanation"}
           id="explanation"
           type="text"
           isInvalid={Boolean(errors.explanation)}
@@ -104,10 +107,11 @@ function ArticleForm({
           {errors.explanation?.message}
         </Form.Control.Feedback>
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Label htmlFor="localDateTime">Date (iso format)</Form.Label>
         <Form.Control
-          data-testid="UCSBDateForm-localDateTime"
+          data-testid={testIdPrefix + "-localDateTime"}
           id="localDateTime"
           type="datetime-local"
           isInvalid={Boolean(errors.localDateTime)}
@@ -117,11 +121,13 @@ function ArticleForm({
           })}
         />
         <Form.Control.Feedback type="invalid">
-          {errors.localDateTime && "LocalDateTime is required. "}
+          {errors.localDateTime && "LocalDateTime is required."}
         </Form.Control.Feedback>
       </Form.Group>
 
-      <Button type="submit">{buttonLabel}</Button>
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
       <Button
         variant="Secondary"
         onClick={() => navigate(-1)}
